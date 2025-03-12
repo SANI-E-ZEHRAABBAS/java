f6f96bc9dc4a69819b945170475e16d89459f734
SANI-E-ZEHRAABBAS <saniezehraabbas41@gmail.com>
2025-03-12 12:48:17 +0500

    done by sani

--- Lab3/Main.java
+++ Lab3/Main.java
@@ -8,7 +8,9 @@ public class Main {
 	 c1.honk();
 	 student s1 = new student();
 	 s1.introduce("Sani", 18);
-
+calculatorforpolymorphism cal1 = new calculatorforpolymorphism();
+System.out.println(cal1.add(100, 50));
+System.out.println(cal1.add(100, 50, 50));
 	 
 	}
 
---
+++ Lab3/calculatorforpolymorphism.java
@@ -0,0 +1,15 @@
+package Lab3;
+
+public class calculatorforpolymorphism {
+
+	public int add(int a, int b) {
+		int res=0;
+		res=a+b;
+		 return res;
+	}
+	public int  add(int a, int b, int c) {
+		int res=0;
+		res=a+b+c;
+		return res;
+	}
+}
